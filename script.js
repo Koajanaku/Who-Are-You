bc3b339f2c35abf661b1a052fd6468d69085fdf9
Koajanaku <koajanaku@cps.edu>
2025-04-07 14:38:32 +0000

    2

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 let input1 = document.querySelector (".questionOne");
 let input2 = document.querySelector (".questionTwo");
+let dad = document.querySelector(".dad");
 let button = document.querySelector ("button");
 
-    let answer1 = input1.value;
-        console.log(answer1);
-
-    let answer2 = input2.value;
-        console.log(answer2);
-    
 
 button.onclick = function(){
     console.log("button check")
+    let answer1 = input1.value;
+    console.log(answer1);
 
-};
+let answer2 = input2.value;
+    console.log(answer2);
 
 
-if(answer1 === "tall" && answer2 === "snack"){
-    console.log("shagy");
+if(answer1=== "tall" && answer2==="snack"){
+console.log("shagy");
+
+
+}
 }
 
 
 
 
 
+console.log("check if js is working");
 
 
 
 
 
 
-console.log("check if js is working")
+
+
+
